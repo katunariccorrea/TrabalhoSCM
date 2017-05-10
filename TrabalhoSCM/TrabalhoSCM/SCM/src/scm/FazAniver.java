3dd5a86e46b0ae212f5f4c3400185c3455745131
katunariccorrea <vinikatunaric@gmail.com>
2017-05-10 20:09:11 -0300

    Alteração tirando acento da classe 10/05/2017

---
+++ TrabalhoSCM/TrabalhoSCM/SCM/src/scm/FazAniver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scm;
+
+/**
+ *
+ * @author katunaric
+ */
+public class FazAniver {
+       public static int fazAniver(int aniversario){
+    aniversario++;    
+    return aniversario;
+    
+    }
+    // Alteração em 10/05/2017.
+}
